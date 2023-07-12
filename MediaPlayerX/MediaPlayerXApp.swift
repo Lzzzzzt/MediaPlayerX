e48023c5c001ceedd62a223224aef4e9ec7a1626
Lzzzt <liuzitao0123@gmail.com>
2023-07-12 16:40:44 +0800

    Initial Commit

---
+++ MediaPlayerX/MediaPlayerXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerXApp.swift
+//  MediaPlayerX
+//
+//  Created by Lzzzt on 2023/7/12.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
